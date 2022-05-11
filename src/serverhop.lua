e9d6abf5daea9d3027b3fb51d3f86845d3ef53ae
dom <dompec07@gmail.com>
2022-05-11 11:19:49 -0400

    Create serverhop.lua

---
+++ src/serverhop.lua
@@ -0,0 +1,15 @@
+-- infyield
+
+local x = {}
+
+for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
+    if type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
+        x[#x + 1] = v.id
+    end
+end
+
+if #x > 0 then
+    game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, x[math.random(1, #x)])
+else
+    return print("Umbra | Couldn't find a server to hop to")
+end
